dc9c2ae6704e08f8da7e43754516528b5e91c159
Nikolay Lukyanov <Nikolay.Lukyanov@Dataart.com>
2020-03-20 17:07:51 +0200

    Custom select bug fix

--- src/components/UI/CustomSelect/CustomSelect.js
+++ src/components/UI/CustomSelect/CustomSelect.js
@@ -1,15 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import classNames from "classnames";
 import './customSelect.scss';
 import siteSettingsContext from "../../../context/siteSettingsContext";
 
 export default function CustomSelect({options, placeholder, name, updated, id, value, selectChanged, hasReset, required, hasErrors}) {
     const { translate } = useContext(siteSettingsContext);
+    const [ isActive, setIsActive ] = useState(false);
+
+    useEffect(() => {
+        setIsActive(false);
+    }, [value]);
 
     return (
         <div className="customSelect">
-            <div className="form__field-holder">
-                <input className={classNames('form__field customSelect__value', {isUpdated: updated, required: required, hasErrors: hasErrors})} type="text" title={name} value={value} readOnly onChange={e => e} />
+            <div className="form__field-holder" onClick={() => setIsActive(true)}>
+                <input className={classNames('form__field customSelect__value', {isUpdated: updated, required: required, hasErrors: hasErrors, isActive: isActive})} type="text" title={name} value={value} readOnly onChange={e => e} />
                 <i className="fa fa-chevron-down customSelect__arrow" />
                 {
                     placeholder ?
@@ -25,24 +30,26 @@ export default function CustomSelect({options, placeholder, name, updated, id, v
                             null
                     }
                     {
-                        options.map(opt => {
-                            return (
-                                <div className="customSelect__drop-opt" key={opt.id} onClick={() => selectOpt(id, opt.id)}>
-                                    {
-                                        opt.icon ?
-                                            <i className={opt.icon} style={{width: 20, textAlign: 'center', marginRight: 5}} />
-                                            :
-                                            null
-                                    }
-                                    { opt.title }
-                                </div>
-                            );
-                        })
+                        options.map(opt => _renderOpt(opt))
                     }
                 </div>
             </div>
         </div>
     );
+
+    function _renderOpt(opt) {
+        return (
+            <div className="customSelect__drop-opt" key={opt.id} onClick={() => selectOpt(id, opt.id)} id={opt.id}>
+                {
+                    opt.icon ?
+                        <i className={opt.icon} style={{width: 20, textAlign: 'center', marginRight: 5}} />
+                        :
+                        null
+                }
+                { opt.title }
+            </div>
+        );
+    }
 
     function selectOpt(fieldID, value) {
         selectChanged(fieldID, value);
