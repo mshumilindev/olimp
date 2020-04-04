3de88466c0e354adaa1479b20244e0fbf96097bd
Nikolay Lukyanov <Nikolay.Lukyanov@Dataart.com>
2020-04-04 11:19:02 +0300

    Fixed custom select functionality

--- src/components/UI/CustomSelect/CustomSelect.js
+++ src/components/UI/CustomSelect/CustomSelect.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect, useRef } from 'react';
 import classNames from "classnames";
 import './customSelect.scss';
 import siteSettingsContext from "../../../context/siteSettingsContext";
@@ -7,6 +7,7 @@ import {Scrollbars} from "react-custom-scrollbars";
 export default function CustomSelect({options, placeholder, name, updated, id, value, selectChanged, hasReset, required, hasErrors}) {
     const { translate } = useContext(siteSettingsContext);
     const [ isActive, setIsActive ] = useState(false);
+    const $currentSelect = useRef(null);
 
     useEffect(() => {
         window.addEventListener('click', e => handleBodyClick(e));
@@ -20,8 +21,8 @@ export default function CustomSelect({options, placeholder, name, updated, id, v
     }, [value]);
 
     return (
-        <div className="customSelect">
-            <div className="form__field-holder" onClick={e => {e.stopPropagation(); setIsActive(!isActive)}}>
+        <div className="customSelect" ref={$currentSelect}>
+            <div className="form__field-holder" onClick={() => setIsActive(!isActive)}>
                 <input className={classNames('form__field customSelect__value', {isUpdated: updated, required: required, hasErrors: hasErrors, isActive: isActive})} type="text" title={name} value={value} readOnly onChange={e => e} />
                 <i className="fa fa-chevron-down customSelect__arrow" />
                 {
@@ -72,6 +73,8 @@ export default function CustomSelect({options, placeholder, name, updated, id, v
     }
 
     function handleBodyClick(e) {
-        setIsActive(false);
+        if ( e.target !== $currentSelect.current && e.target.closest('.customSelect') !== $currentSelect.current ) {
+            setIsActive(false);
+        }
     }
 }
