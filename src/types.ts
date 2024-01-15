b6a49c26d5908a68659f81e1046048362cdea103
Nikolay Lukyanov <nikolay.frankovich@gmail.com>
2024-01-15 16:15:24 +0200

    Typings

--- src/types.ts
+++ src/types.ts
@@ -22,3 +22,26 @@ export type User = {
 };
 
 export type Course = {};
+
+export type IUpdate = 'translations' | 'siteSettings';
+
+export type ILang = {
+  en: string;
+  ru: string;
+  ua: string;
+};
+
+export type ITranslation = {
+  id: string;
+  langs: ILang[];
+};
+
+export type ISiteSettings = {
+  address: {
+    value: string;
+  },
+  logo: {
+    url: string;
+  },
+  siteName: ILang;
+};
